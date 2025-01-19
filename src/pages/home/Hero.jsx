026904d2d16f2f1d6bfdac25f67b51901fe21b13
Bidhan Roy <bidhanroy578@gmail.com>
2025-01-19 22:22:54 +0600

    made nav bar responsive for mobile device

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -3,13 +3,13 @@
 const Hero = () => {
     return (
         <div className="my-10">
-            <div className="bg-[#F3F3F3] w-3/4 mx-auto flex items-center content-center border p-5">
-                <div className="w-2/3 border border-black ">
-                    <h3 className="font-bold text-6xl text-black">Books to freshen up your bookshelf</h3>
-                    <button className="btn btn-green">View The List</button>
+            <div className="bg-[#F3F3F3] rounded-3xl mx-auto flex items-center content-center border p-5">
+                <div className="w-2/3 md:pl-20">
+                    <h3 className="font-bold text-6xl text-black text-balance">Books to freshen up your bookshelf</h3>
+                    <button className="btn px-7 text-lg font-semibold bg-[#23BE0A] hover:bg-green-400 text-white mt-9">View The List</button>
                 </div>
 
-                <div className="w-1/3 border border-black "><img src="/pngwing1.png" alt="book" /></div>
+                <div className="w-1/3 "><img src="/pngwing1.png" alt="book" /></div>
 
             </div>
         </div>
--- src/pages/nav/Nav.jsx
+++ src/pages/nav/Nav.jsx
@@ -1,21 +1,35 @@
 import { NavLink, Outlet } from "react-router";
+import { CiMenuKebab } from "react-icons/ci";
 import "./Nav.css"
+import { useState } from "react";
 
 const Nav = () => {
+    let [cl, setCl] = useState(false)
     let linklist = <>
-                    <NavLink className={'btn font-semibold text-lg shadow-none hover:bg-green-200 bg-transparent border-none ouline-none'} to={'/'}>Home</NavLink>
-                    <NavLink className={'btn font-semibold text-lg shadow-none hover:bg-green-200 bg-transparent border-none ouline-none'} to={'/listed-books'}>Listed Books</NavLink>
-                    <NavLink className={'btn font-semibold text-lg shadow-none hover:bg-green-200 bg-transparent border-none ouline-none'} to={'/pages-to-read'}>Pages to Read</NavLink>
+        <NavLink onClick={()=> setCl(false)} className={'btn font-semibold text-xl  lg:text-lg shadow-none hover:bg-green-200 bg-transparent border-none ouline-none'} to={'/'}>Home</NavLink>
+        <NavLink onClick={()=> setCl(false)} className={'btn font-semibold text-xl  lg:text-lg shadow-none hover:bg-green-200 bg-transparent border-none ouline-none'} to={'/listed-books'}>Listed Books</NavLink>
+        <NavLink onClick={()=> setCl(false)} className={'btn font-semibold text-xl  lg:text-lg shadow-none hover:bg-green-200 bg-transparent border-none ouline-none'} to={'/pages-to-read'}>Pages to Read</NavLink>
     </>
     return (
         <div className="my-10">
-            <div className="grid grid-cols-3 ">
+            <div className="flex justify-between items-center lg:grid grid-cols-3 ">
                 <div className="font-bold text-[28px] btn border-none justify-start bg-transparent shadow-none hover:bg-transparent hover:">Book Vibe</div>
-                <div className="flex gap-2"> {linklist} </div>
-                <div className="text-end">
-                    <button className="btn mx-2 px-7 text-lg bg-[#23BE0A] text-white">Sign In</button>
-                    <button className="btn mx-2 px-7 text-lg bg-[#59C6D2] text-white">Sign Up</button></div>
+                <div className=" hidden lg:flex gap-2"> {linklist} </div>
+                <div className="text-end hidden lg:inline-block">
+                    <button className="btn mx-2 px-7 text-lg bg-[#23BE0A] hover:bg-green-400 text-white">Sign In</button>
+                    <button className="btn mx-2 px-7 text-lg hover:bg-[#59c6d2a6] bg-[#59C6D2] text-white">Sign Up</button>
+                </div>
+                {/* this is for mobile view  */}
+                <div onClick={()=> setCl(!cl)} className="md:hidden">
+                   <div className="btn bg-transparent shadow-none hover:bg-green-200"><CiMenuKebab /></div>
+                </div>
             </div>
+            <div className={`duration-300 ${cl ? '' : 'hidden'}`}>
+                <div className="h-full w-full bg-white absolute z-10 bg-opacity-70 "></div>
+                <div className="rounded-t-xl absolute z-20 w-full p-5 flex flex-col ">{linklist}</div>
+            </div>
+
+
             <Outlet></Outlet>
         </div>
     );
