d37028b394c1dbbcb4d52b8e919d1af0cc8aedab
Bidhan Roy <bidhanroy578@gmail.com>
2025-01-20 02:19:06 +0600

    error page updated2

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 const ErrorPage = () => {
     return (
         <div className='font-bold text-3xl text-center'>
-            this is for error page <br />
+            Page not found ... <br />
             <Link to={'/'}><button className='btn btn-error'>go back to home page</button></Link> 
         </div>
     );
